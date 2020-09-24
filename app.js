ea4dceebddf4270328c2d6ad66b1c3a2996af3ef
Heather Everton <heather.everton@planplusonline.com>
2020-09-24 12:01:10 -0600

    update project question inputs

--- app.js
+++ app.js
@@ -73,20 +73,17 @@ const promptProject = portfolioData => {
         message: 'Would you like to enter another project?',
         default: false
       }
+    ])
 
-      .then(projectData => {
-        portfolioData.projects.push(projectData)
-        .then(projectData => {
-            portfolioData.projects.push(projectData);
-            if (projectData.confirmAddProject) {
-              return promptProject(portfolioData);
-            } else {
-              return portfolioData;
-            }
-          });
-      })
-    ]);
-  };
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+        });
+};
   
   promptUser()
   .then(promptProject)
